33c21a660142a7effac896b0cf76dfacfe0ab616
AmitBishnoi2 <86775891+AmitBishnoi2@users.noreply.github.com>
2021-07-02 20:00:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var b = prompt ('enter 2nd variable ')
+var a = prompt('Enter the first variable: ');
+
+function setup(){
+  var b2 = createButton("click here to Swap");
+b2.mousePressed(swap)
+  }
+
+
+function draw()
+{ 
+}
+function swap ()
+{
+  
+  [a,b]=[b,a]
+console.log (a)
+console.log (b)
+}
